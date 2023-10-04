8818f67c93f814d9f9c7724fe4c9011c9a8fe59e
Your Name <dylan.7mcdougall@gmail.com>
2023-10-03 17:54:22 -0700

    Added functionality for User login in the backend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,26 +1,21 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
-
 // GET /api/restore-user
-
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+  });
 
 router.get(
   '/restore-user',
@@ -29,9 +24,6 @@ router.get(
   }
 );
 
-router.use(restoreUser);
-
-// ...
 
 // GET /api/require-auth
 const { requireAuth } = require('../../utils/auth.js');
