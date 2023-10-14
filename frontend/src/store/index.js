3fa7ad7cadba12e58f7d27fe52b2e58b66c81000
Dylan <dylan.7mcdougall@gmail.com>
2023-10-14 11:39:41 -0700

    resolved redux logger bug

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
-import { logger } from 'redux-logger'
 import { restoreCSRF, csrfFetch } from './csrf';
 
 const rootReducer = combineReducers({
@@ -14,6 +13,7 @@ let enhancer = applyMiddleware(thunk);
 if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
+  const logger = require('redux-logger');
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
