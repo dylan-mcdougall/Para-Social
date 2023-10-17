2640bc0b2535b14fe0f444bf0d310163e89452ed
Dylan <dylan.7mcdougall@gmail.com>
2023-10-16 17:30:16 -0700

    created shell for room messages

--- frontend/src/components/RoomDisplay/index.js
+++ frontend/src/components/RoomDisplay/index.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import RoomMessages from '../RoomMessages';
 import './RoomDisplay.css';
 
 function RoomDisplay({ isLoaded }) {
     const dispatch = useDispatch();
     const room = useSelector(state => state.room.room);
     
-    
     return (
         <div className='room-display-wrapper'>
-            {room.name}
+            {isLoaded && (
+                <>
+                {room?.name}
+                <div>
+                    <RoomMessages isLoaded={isLoaded} />
+                </div>
+                </>
+            )}
         </div>
     )
 }
--- frontend/src/components/RoomMessages/index.js
+++ frontend/src/components/RoomMessages/index.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './RoomMessages';
+import './RoomMessages.css';
 
-function RoomMessages() {
-
+function RoomMessages({ isLoaded }) {
+    const room = useSelector(state => state.room.room);
 
     return (
         <>
+            {isLoaded && (
+                <div className='room-messages-wrapper'>
+                    <ul className='room-messages-ul'>
+                    {room?.Messages?.map((message) => {
+                        return (
+                            <li className='message-item' key={message.id}>
+                                {message.content_message}
+                            </li>
+                        )
+                    })}
+                    </ul>
+                </div>
+            )}
         </>
     )
 }
