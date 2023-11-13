70a6161ad4e9157e65716baa2c8f1e92b52903bb
Dylan <dylan.7mcdougall@gmail.com>
2023-11-13 14:04:30 -0800

    updated database to provide classifications for rooms to provide intended UX

--- backend/db/seeders/20231007182142-demo-rooms.js
+++ backend/db/seeders/20231007182142-demo-rooms.js
@@ -11,40 +11,76 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         community_id: 1,
-        name: 'Server Chat'
+        name: 'Server Chat',
+        classification: 'text'
       }, {
         community_id: 1,
-        name: 'Degens'
+        name: 'Degens',
+        classification: 'text'
       }, {
         community_id: 1,
-        name: 'Tea Time'
+        name: 'Tea Time',
+        classification: 'text'
       }, {
         community_id: 1,
-        name: 'Music'
+        name: 'Music',
+        classification: 'text'
       }, {
         community_id: 1,
-        name: 'Gaming'
+        name: 'Gaming',
+        classification: 'text'
       }, {
         community_id: 1,
-        name: 'Updates'
+        name: 'Updates',
+        classification: 'text'
+      }, {
+        community_id: 1,
+        name: 'About and Rules',
+        classification: 'about'
+      }, {
+        community_id: 1,
+        name: 'Announcements',
+        classification: 'announcement'
       }, {
         community_id: 2,
-        name: 'Server Chat'
+        name: 'Server Chat',
+        classification: 'text'
       }, {
         community_id: 2,
-        name: 'Music'
+        name: 'Music',
+        classification: 'text'
       }, {
         community_id: 2,
-        name: 'Gaming'
+        name: 'Gaming',
+        classification: 'text'
       }, {
         community_id: 2,
-        name: 'Updates'
+        name: 'Updates',
+        classification: 'text'
+      }, {
+        community_id: 2,
+        name: 'About and Rules',
+        classification: 'about'
+      }, {
+        community_id: 2,
+        name: 'Announcements',
+        classification: 'announcement'
       }, {
         community_id: 3,
-        name: 'Server Chat'
+        name: 'Server Chat',
+        classification: 'text'
       }, {
         community_id: 3,
-        name: 'Updates'
+        name: 'Updates',
+        classification: 'text'
+      }, {
+        community_id: 3,
+        name: 'About and Rules',
+        classification: 'about'
+      }, {
+        community_id: 3,
+        name: 'Announcements',
+        classification: 'announcement'
       }
     ], { validate: true });
   },
