bba1439edc390737c667895d882315621d29d41f
Dylan <dylan.7mcdougall@gmail.com>
2024-01-04 16:02:58 -0800

    added footer to landing page as well as initial mobile styling

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -4,6 +4,7 @@ import { Redirect } from 'react-router-dom';
 import './LandingPage.css';
 import LoginFormPage from '../LoginFormPage';
 import SignupFormPage from '../SignupFormPage';
+import Footer from '../Footer';
 
 function LandingPage() {
     const sessionUser = useSelector(state => state.session.user);
@@ -25,17 +26,22 @@ function LandingPage() {
     }
     
     return (
-        <div className='landing-page-wrapper'>
-            <div className='landing-page-left'>
-                <img className='landing-left-logo' src='https://aaprojectbucket.s3.us-west-1.amazonaws.com/Para-Social-Logo.png' alt='Logo' />
-                {/* <img className='landing-left-image' src='https://aaprojectbucket.s3.us-west-1.amazonaws.com/influencers.jpg' alt='Demographic Image' /> */}
-            </div>
-            <div className='landing-page-right'>
-                <div className='landing-right-flex'>
-                    {landingRight}
+        <>
+            <div className='landing-page-wrapper'>
+                <div className='landing-page-left'>
+                    <img className='landing-left-logo' src='https://aaprojectbucket.s3.us-west-1.amazonaws.com/Para-Social-Logo.png' alt='Logo' />
+                    {/* <img className='landing-left-image' src='https://aaprojectbucket.s3.us-west-1.amazonaws.com/influencers.jpg' alt='Demographic Image' /> */}
+                    <img className='landing-left-logo mobile' src='https://aaprojectbucket.s3.us-west-1.amazonaws.com/FullLogo_Transparent_NoBuffer_resized.png' alt='Logo' />
+                    <img className='landing-left-title mobile' src='https://aaprojectbucket.s3.us-west-1.amazonaws.com/FullLogo_Title.png' alt='Title' />
+                </div>
+                <div className='landing-page-right'>
+                    <div className='landing-right-flex'>
+                        {landingRight}
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
